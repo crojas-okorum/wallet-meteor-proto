ccbae0e07377af9dd9187b6a2110582d19631e20
Cristian Rojas <cristian.rojas@okorum.com>
2023-01-23 09:22:50 -0500

    Tailwind instalado

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,7 +5,9 @@ import { ContactList } from './ContactList';
 
 export const App = () => (
   <div>
-    <h1>Meteor Wallet</h1>
+   <h1 className="text-3xl text-indigo-800">
+      Welcome to Meteor + Tailwind!
+    </h1>
     <ContactForm />
     <ContactList />
   </div>
